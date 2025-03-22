befd0ba09bb38a553bab95bc3a4383d5278cb9c2
Taiwei <ktuou207@gmail.com>
2025-03-22 22:59:33 +0800

    chore: adjust eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,90 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  pluginJs.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
   {
-    ignores: ['eslint.config.mjs'],
+    files: [
+      'src/**/*.{js,mjs,cjs,ts}',
+      'eslint.config.mjs',
+    ],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/quotes': [
+        'error',
+        'single',
+      ],
+      '@stylistic/indent': [
+        'error',
+        2,
+      ],
+      '@stylistic/comma-dangle': [
+        'error',
+        'always-multiline',
+      ],
+      '@stylistic/semi': [
+        'error',
+        'never',
+      ],
+      '@stylistic/array-bracket-newline': [
+        'error',
+        { minItems: 2 },
+      ],
+      '@stylistic/array-element-newline': [
+        'error',
+        { minItems: 2 },
+      ],
+      '@stylistic/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'comma',
+            requireLast: true,
+          },
+          singleline: { delimiter: 'comma' },
+        },
+      ],
+      '@stylistic/object-property-newline': 'error',
+      '@stylistic/object-curly-newline': [
+        'error',
+        {
+          ImportDeclaration: 'always',
+          ObjectPattern: {
+            multiline: true,
+            consistent: true,
+          },
+          ObjectExpression: {
+            multiline: true,
+            consistent: true,
+          },
+        },
+      ],
+      '@stylistic/arrow-parens': [
+        'error',
+        'as-needed',
+      ],
+      '@stylistic/brace-style': [
+        'error',
+        '1tbs',
+      ],
+      'no-param-reassign': [
+        'error',
+        { props: true },
+      ],
+      'arrow-body-style': [
+        'error',
+        'as-needed',
+      ],
+      'no-unneeded-ternary': 'error',
+      'no-nested-ternary': 'error',
+    },
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -20,16 +94,20 @@ export default tseslint.config(
       ecmaVersion: 5,
       sourceType: 'module',
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: [
+            'eslint-config-myconfig.mjs',
+            'eslint.config.mjs',
+          ],
+          defaultProject: 'tsconfig.json',
+        },
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-extraneous-class': 'off',
     },
-  },
-);
+  }
+)
