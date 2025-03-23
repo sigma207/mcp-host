6cc52e3f634117fc5aa5ca4f253a110a21ed6025
Taiwei <ktuou207@gmail.com>
2025-03-23 09:53:14 +0800

    chore: add swagger

--- src/dto/completions.dto.ts
+++ src/dto/completions.dto.ts
@@ -1,3 +1,11 @@
+import {
+  ApiProperty, 
+} from '@nestjs/swagger'
 export class CreateCompletionsDto {
+  @ApiProperty({
+    type: 'string',
+    example: 'What is the capital of Taiwan?',
+    description: 'The message to send to the model',
+  })
   message: string
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import {
   NestFactory, 
 } from '@nestjs/core'
 import {
+  SwaggerModule, DocumentBuilder, 
+} from '@nestjs/swagger'
+import {
   AppModule, 
 } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Mcp Host')
+    .setDescription('The MCP host API description')
+    .setVersion('1.0')
+    .addTag('mcp')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
   await app.listen(process.env.PORT ?? 8000)
 }
 bootstrap()
