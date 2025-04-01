48242d69a6da1cb53bb8238da71e97e2ab46448d
TAIWEI.LEE <taiwei.lee@deltaww.com>
2025-04-01 09:44:14 +0800

    style: adjust format by ESLint

--- src/utils/chroma.ts
+++ src/utils/chroma.ts
@@ -1,15 +1,21 @@
-import crypto from "crypto";
-import { OllamaEmbeddingFunction } from "chromadb";
-import { Collection, type AddRecordsParams } from "chromadb";
-import type { ResourceContents, TextResourceContents } from "@modelcontextprotocol/sdk/types.js";
+import crypto from 'crypto'
+import {
+  OllamaEmbeddingFunction, 
+} from 'chromadb'
+import {
+  Collection, type AddRecordsParams, 
+} from 'chromadb'
+import type {
+  ResourceContents, TextResourceContents, 
+} from '@modelcontextprotocol/sdk/types.js'
 
 //mxbai-embed-large
 export const mxbaiEmbeddingFunction = new OllamaEmbeddingFunction({
-  model: "mxbai-embed-large",
-  url: "http://10.137.2.13:11434/api/embeddings",
-});
+  model: 'mxbai-embed-large',
+  url: 'http://10.137.2.13:11434/api/embeddings',
+})
 const isTextResourceContents = (resource: ResourceContents): resource is TextResourceContents =>
-  resource.mimeType === "text/plain" || resource.mimeType === "application/json";
+  resource.mimeType === 'text/plain' || resource.mimeType === 'application/json'
 
 export const resourcesToDocuments = async (
   name: string,
@@ -20,23 +26,23 @@ export const resourcesToDocuments = async (
     ids: [],
     documents: [],
     metadatas: [],
-  };
-  mcpServerAllResourceContents.forEach((resource) => {
+  }
+  mcpServerAllResourceContents.forEach(resource => {
     if (isTextResourceContents(resource)) {
-      documents.ids.push(crypto.randomUUID());
+      documents.ids.push(crypto.randomUUID())
       const metadatas = {
         name,
         uri: resource.uri,
-        mimeType: "",
-      };
-      if (typeof resource.mimeType === "string") {
-        metadatas.mimeType = resource.mimeType;
+        mimeType: '',
       }
-      documents.metadatas?.push(metadatas);
-      documents.documents.push(resource.text);
+      if (typeof resource.mimeType === 'string') {
+        metadatas.mimeType = resource.mimeType
+      }
+      documents.metadatas?.push(metadatas)
+      documents.documents.push(resource.text)
     }
     // console.log('resource', resource)
-  });
+  })
   // console.log(documents)
-  await collection.add(documents);
-};
+  await collection.add(documents)
+}
